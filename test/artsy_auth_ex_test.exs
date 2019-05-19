4970d9919f6f00d7375cd1f0dcb5d6a99b6320c9
ashkan18 <ashkan18@gmail.com>
2019-05-19 17:52:02 -0400

    Fix non-existing tests

--- test/artsy_auth_ex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ArtsyAuthExTest do
-  use ExUnit.Case
-  doctest ArtsyAuthEx
-
-  test "greets the world" do
-    assert ArtsyAuthEx.hello() == :world
-  end
-end
---
+++ test/auth/plug_test.exs
@@ -0,0 +1,4 @@
+defmodule Artsy.Auth.PlugTest do
+  use ExUnit.Case
+  doctest Artsy.Auth.Plug
+end
